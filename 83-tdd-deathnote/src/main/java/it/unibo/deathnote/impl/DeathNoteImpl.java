6cb0c8861e86d919a6d2e27a1c78c58fbd5d300a
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2023-11-13 23:35:40 +0100

    new implementation

--- 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
+++ 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -5,53 +5,82 @@ import it.unibo.deathnote.api.DeathNote;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class DeathNoteImpl implements DeathNote {
 
-    private Set<String> names;
+    final private Set<String> names;
+    private String lastName;
 
-    @Override
-    public String getRule(int ruleNumber) {
-        // TODO Auto-generated method stub
-        throw new IllegalArgumentException("Cannot access rule number 0 and negative rules as they do not exist");
+    class Human {
+        final private String name;
+        final private String cause;
+        final private String details;
+
+        public Human(final String name, final String cause, final String details) {
+            this.name = name;
+            this.cause = cause;
+            this.details = details;
+        }
+    }
+
+    public DeathNoteImpl() {
+        names = new HashSet<String>();
     }
 
     @Override
-    public void writeName(String name) {
-        // TODO Auto-generated method stub
-        throw new IllegalArgumentException("Cannot insert the same person");
+    public String getRule(final int ruleNumber) {
+        try {
+            return DeathNote.RULES.get(ruleNumber);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("Cannot access rule number 0 and negative rules as they do not exist");
+        }
     }
 
     @Override
-    public boolean writeDeathCause(String cause) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+    public void writeName(final String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Cannot insert null string inside death note");
+        }
+        if (name.equals("")) {
+            throw new IllegalArgumentException("Cannot insert empty string inside death note");
+        }
+        if (!names.add(name)) {
+            throw new IllegalArgumentException("Cannot insert the same person");
+        }
     }
 
     @Override
-    public boolean writeDetails(String details) {
+    public boolean writeDeathCause(final String cause) {
+        
+    }
+
+    @Override
+    public boolean writeDetails(final String details) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
     }
 
     @Override
-    public String getDeathCause(String name) {
+    public String getDeathCause(final String name) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
     }
 
     @Override
-    public String getDeathDetails(String name) {
+    public String getDeathDetails(final String name) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
     }
 
     @Override
-    public boolean isNameWritten(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+    public boolean isNameWritten(final String name) {
+        return names.contains(name);
     }
     
 }
