3d59adfa3da90c57e5108af42280a0af93e8f4dc
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2023-11-13 20:33:04 +0100

    Wrote test 1

--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,10 +1,32 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImpl;
+
 class TestDeathNote {
+
+    private DeathNote deathNote;
+
+    @BeforeEach
+    public void setUp() {
+        deathNote = new DeathNoteImpl();    
+    }
+
+
     @Test
     public void testRuleZeroAndNegativeRules() {
-        
+        try {
+            deathNote.getRule(0);
+            deathNote.getRule(-1);
+            fail("Rules 0 and -1 exist");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Cannot access rule number 0 and negative rules as they do not exist", e.getMessage());
+        }
     }
 }
