b0bbef61eccd5fb3e95f56a1aa89c6c8f6fa07d4
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2023-11-14 17:02:51 +0100

    fixed DeathNoteImpl for testCauseOfDeath()

--- 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
+++ 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -16,6 +16,8 @@ import java.util.Set;
 public class DeathNoteImpl implements DeathNote {
 
     final private Set<Human> humans;
+    private Human lastHumanWritten;
+    private long timeLastHumanWritten;
 
     public DeathNoteImpl() {
         humans = new HashSet<Human>();
@@ -38,13 +40,24 @@ public class DeathNoteImpl implements DeathNote {
         if (name.equals("")) {
             throw new IllegalArgumentException("Cannot insert empty string inside death note");
         }
-        if (!humans.add(new Human(name))) {
+        final Human hNew = new Human(name);
+        if (!humans.add(hNew)) {
             throw new IllegalArgumentException("Cannot insert the same person");
         }
+        lastHumanWritten = hNew;
+        timeLastHumanWritten = System.currentTimeMillis();
     }
 
     @Override
     public boolean writeDeathCause(final String cause) {
+        if (lastHumanWritten == null) {
+            throw new IllegalStateException("Cannot write cause of death before a name has been written");
+        }
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - timeLastHumanWritten < 40) {
+            lastHumanWritten.setCause(cause);
+            return true;
+        }
         return false;
     }
 
@@ -56,8 +69,16 @@ public class DeathNoteImpl implements DeathNote {
 
     @Override
     public String getDeathCause(final String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+        Human h = new Human(name);
+        for (Human k : humans) {
+            if (k.equals(h)) {
+                if (k.getCause().equals("")) {
+                    return "heart attack";
+                }
+                return k.getCause();
+            }
+        }
+        throw new IllegalArgumentException("Person does not exist inside death note");
     }
 
     @Override
